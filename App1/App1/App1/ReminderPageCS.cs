12be52400f3ce268edeb0916731eb326b252d818
Foyzul Karim <foyzulkarim@gmail.com>
2016-09-17 17:11:46 +0600

    initial commit

---
+++ App1/App1/App1/ReminderPageCS.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace App1
+{
+    public class ReminderPageCS : ContentPage
+    {
+        public ReminderPageCS()
+        {
+            Title = "Reminder Page";
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label {
+                        Text = "Reminder data goes here",
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.CenterAndExpand
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ App1/App1/App1/TodoListPageCS.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace App1
+{
+    public class TodoListPageCS : ContentPage
+    {
+        public TodoListPageCS()
+        {
+            Title = "TodoList Page";
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label {
+                        Text = "Todo list data goes here",
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.CenterAndExpand
+                    }
+                }
+            };
+        }
+    }
+}
