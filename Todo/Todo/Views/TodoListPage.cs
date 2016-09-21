e05686ff85134c6fa9917fd15b1b9a23f55d3f6f
Foyzul Karim <foyzulkarim@gmail.com>
2016-09-21 23:45:40 +0600

    applied stylecop

--- Todo/Todo/Views/TodoListPage.cs
+++ Todo/Todo/Views/TodoListPage.cs
@@ -20,7 +20,7 @@ namespace Todo
                 var todoPage = new TodoItemPage();
                 todoPage.BindingContext = todoItem;
 
-                ((App)App.Current).ResumeAtTodoId = todoItem.ID;
+                ((App)Application.Current).ResumeAtTodoId = todoItem.ID;
                 Debug.WriteLine("setting ResumeAtTodoId = " + todoItem.ID);
 
                 Navigation.PushAsync(todoPage);
